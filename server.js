6efab1e51d92f20e61d1f926e7ae3327987af9d0
Abhishek <bumblebee31304@gmail.com>
2023-12-26 13:17:14 +0530

    feat: delete functionality

--- server.js
+++ server.js
@@ -1,34 +1,50 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ShortUrl = require('./models/shortUrl')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+const app = express();
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
-})
+});
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
-})
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
-
-  res.redirect('/')
-})
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect('/');
+});
 
 app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
 
-  shortUrl.clicks++
-  shortUrl.save()
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+});
 
-  res.redirect(shortUrl.full)
-})
+app.delete('/deleteUrl/:urlId', async (req, res) => {
+  const urlId = req.params.urlId;
 
-app.listen(process.env.PORT || 5000);
+  try {
+    const deletedUrl = await ShortUrl.findByIdAndDelete(urlId);
+
+    if (deletedUrl) {
+      res.status(200).json({ message: 'URL deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'URL not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server is running...');
+});
